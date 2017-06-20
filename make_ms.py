2e88e3333c7d43c043e7697bd735df6a70576d27
Ian Heywood <ian.heywood@gmail.com>
2017-06-20 15:15:17 +0100

    Python

---
+++ make_ms.py
@@ -0,0 +1,94 @@
+import numpy
+import pylab
+import pickle
+from obsparams import *
+c = 299792458.0 # speed of light
+
+# def getAntennaPositions(myarray):
+# 	if myarray == 'askap':
+# 		xx,yy,zz,dishDiameter = pickle.load(open('askap_antennas.p','rb'))
+# 		nDishes = len(xx)
+# 		coordsys = 'local'
+# 		dishDiameters = numpy.zeros((nDishes,), numpy.float64) + dishDiameter
+# 		dishDiameters = dishDiameters.tolist()
+# 	elif myarray == 'meerkat':
+# 		xx,yy,zz,dishDiameter = pickle.load(open('meerkat_antennas.p','rb'))	
+# 		nDishes = len(xx)
+# 		coordsys = 'local'
+# 		dishDiameters = numpy.zeros((nDishes,), numpy.float64) + dishDiameter
+# 		dishDiameters = dishDiameters.tolist()	
+# 	elif myarray == 'ska':
+# 		xx,yy,zz,dishDiameters = pickle.load(open('ska_antennas.p','rb'))	
+# 		coordsys = 'global'	
+# 	return xx,yy,zz,dishDiameters,coordsys
+	
+def sim_ms(prefix,myarray,minFreq,nChan,chanWidth,startTime,scanLength,xx,yy,zz,dishDiameters,coordsys):
+	projectName = prefix+'_'+myarray+'_'+minFreq+'_'+str(nChan)+'ch_'+chanWidth+'_'+str(int(scanLength/60.0))+'min'
+	RA_central = '19h39m25.027s' 
+	Dec_central = '-63d42m45.63s'
+	dishDiameter = 25.0
+	Stokes = 'XX XY YX YY'
+	integrationTime = '120s'
+	refTime = me.epoch('IAT','2018/01/01')
+	elevationLimit = '8.0deg'
+	shadowLimit = 0.001
+	#xx,yy,zz,dishDiameters,coordsys = getAntennaPositions(myarray)
+	# nDishes = len(xx)
+	# dishDiameters = numpy.zeros((nDishes,), numpy.float64) + dishDiameter
+	# dishDiameters = dishDiameters.tolist()
+	clName = projectName+'.cl'
+	msName = projectName+'.ms'
+	refRA = qa.unit(RA_central)
+	refDec = qa.unit(Dec_central)
+	direction = me.direction(rf = 'J2000', v0 = refRA, v1 = refDec)
+	cl.addcomponent(dir = direction, flux = 1.0, freq = minFreq)
+	cl.rename(filename = clName)
+	cl.done()
+	sm.open(msName)
+	sm.setspwindow(spwname = prefix,
+		freq = minFreq,
+		deltafreq = chanWidth,
+		freqresolution = chanWidth,
+		nchannels = nChan,
+		stokes = Stokes)
+	if myarray == 'askap':
+		obsPosition = me.observatory('ATCA')
+	else:
+		obsPosition = me.observatory('MeerKAT')
+	sm.setconfig(telescopename = myarray,
+		x = xx,
+		y = yy,
+		z = zz,
+		dishdiameter = dishDiameters,
+		mount = 'ALT-AZ',
+		coordsystem = coordsys,
+		referencelocation = obsPosition)
+	sm.setfeed(mode = 'perfect X Y')
+	sm.setfield(sourcename = prefix,
+		sourcedirection = me.direction(rf = 'J2000', v0 = refRA, v1 = refDec))
+	sm.settimes(integrationtime = integrationTime,
+		usehourangle = True,
+		referencetime = refTime)
+	sm.setlimits(shadowlimit = shadowLimit,
+		elevationlimit = elevationLimit)
+	sm.setauto(autocorrwt = 0.0)
+	scan = 0
+	endTime = startTime + scanLength
+	while (startTime < endTime):
+		sm.observe(prefix, prefix, starttime = str(startTime)+'s', stoptime = str(startTime + scanLength)+'s')
+		me.doframe(refTime)
+		me.doframe(obsPosition)
+		hadec = me.direction('hadec', qa.time(str(startTime + scanLength / 2)+'s'), refDec)
+		azel = me.measure(hadec,'azel')
+		sm.setdata(msselect = 'SCAN_NUMBER==' + str(scan))
+		sm.predict(complist = clName)
+		startTime = startTime + scanLength
+		scan += 1
+	sm.done()
+	os.system('rm -rf ' + clName)
+
+	setjy(vis=msName,standard='Perley-Butler 2010',usescratch=True)
+
+	return msName
+
+sim_ms(prefix,myarray,minFreq,nChan,chanWidth,startTime,scanLength,xx,yy,zz,dishDiameters,coordsys)
---
+++ master.py
@@ -0,0 +1,118 @@
+# PKS B1934-638 bandpass simulations
+
+import numpy
+import pickle
+import Pyxis
+import ms
+import mqt
+import os
+
+do_steps = [0,1,2,3]
+
+def pokeTDLconf(filename,param,value):
+	lines = []
+	f = open(filename,'r')
+	line = f.readline()
+	while line:
+		lines.append(line.rstrip('\n'))
+		line = f.readline()
+	f.close()
+	f = open(filename,'w')
+	for line in lines:
+		parts = line.replace(' ','').split('=')
+		if parts[0] == param:
+			print >>f,parts[0]+' = '+value
+		else:
+			print >>f,line
+	f.close()
+
+def getAntennaPositions(myarray):
+	if myarray == 'askap':
+		xx,yy,zz,dishDiameter = pickle.load(open('askap_antennas.p','rb'))
+		nDishes = len(xx)
+		coordsys = 'local'
+		dishDiameters = numpy.zeros((nDishes,), numpy.float64) + dishDiameter
+		dishDiameters = dishDiameters.tolist()
+	elif myarray == 'meerkat':
+		xx,yy,zz,dishDiameter = pickle.load(open('meerkat_antennas.p','rb'))	
+		nDishes = len(xx)
+		coordsys = 'local'
+		dishDiameters = numpy.zeros((nDishes,), numpy.float64) + dishDiameter
+		dishDiameters = dishDiameters.tolist()	
+	elif myarray == 'ska':
+		xx,yy,zz,dishDiameters = pickle.load(open('ska_antennas.p','rb'))	
+		coordsys = 'global'	
+	return xx,yy,zz,dishDiameters,coordsys
+
+def write_obsparams(prefix,
+	myarray,
+	minFreq,
+	nChan,
+	chanWidth,
+	startTime,
+	scanLength):
+	myarray = myarray.lower()
+	projectName = prefix+'_'+myarray+'_'+minFreq+'_'+str(nChan)+'ch_'+chanWidth+'_'+str(int(scanLength/60.0))+'min'
+	msName = projectName+'.ms'
+	bpTable = 'cal_'+msName+'.B'
+	xx,yy,zz,dishDiameters,coordsys = getAntennaPositions(myarray)
+	f = open('obsparams.py','w')
+	print >>f,'prefix="'+str(prefix)+'"'
+	print >>f,'myarray="'+str(myarray)+'"'
+	print >>f,'minFreq="'+str(minFreq)+'"'
+	print >>f,'nChan='+str(nChan)
+	print >>f,'chanWidth="'+str(chanWidth)+'"'
+	print >>f,'startTime='+str(startTime)
+	print >>f,'scanLength='+str(scanLength)
+	print >>f,'projectName="'+str(projectName)+'"'
+	print >>f,'msName="'+str(msName)+'"'
+	print >>f,'bpTable="'+str(bpTable)+'"'
+	print >>f,'xx='+str(xx)
+	print >>f,'yy='+str(yy)
+	print >>f,'zz='+str(zz)
+	print >>f,'dishDiameters='+str(dishDiameters)
+	print >>f,'coordsys="'+str(coordsys)+'"'
+	f.close()
+
+write_obsparams('bpsims',
+	'meerkat',
+	'0.58GHz',
+	32,
+	'1MHz',
+	-150,
+	300)
+
+# step = 0 
+# Read in obsparams file
+if 0 in do_steps:
+	from obsparams import *
+
+# step = 1
+# Execute make_ms.py CASA script (also reads in obsparams)
+# End product is a MS with 1934-638 model in MODEL_DATA
+if 1 in do_steps:
+	os.system('casa -c make_ms.py --nologger --log2term --nogui')
+
+# step = 2
+# Use MeqTrees to add field source visibilities to MODEL_DATA
+# Write result to DATA
+if 2 in do_steps:
+	tdl_sizes = str(dishDiameters).replace('[','').replace(']','').replace(',',' ')
+	pokeTDLconf('tdlconf.profiles','analytic_beams.wsrt_cos3_beam.dish_sizes',tdl_sizes)
+	mqt.run('turbo-sim.py',
+	job='simulate',
+	config='tdlconf.profiles',
+	section='bp_sims',
+	args=['ms_sel.msname='+msName,
+		'tiggerlsm.filename=PKS_B1934-638_ATCA_BETA.lsm.html',
+		'ms_sel.tile_size=16'])
+
+# step = 3
+# Generate a bandpass table deriving corrections for DATA against MODEL_DATA 
+if 3 in do_steps:
+	cc = 'bandpass('
+	cc+= 'vis="'+msName+'",'
+	cc+= 'caltable="'+bpTable+'",'
+	cc+= 'solnorm=True,'
+	cc+= 'solint="inf")'
+	std.runcasapy(cc)
